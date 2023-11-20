170b08e7ff1fecb0f1c19d4f8a25c604186b1105
Ian Hart <ianhart7790@gmail.com>
2023-11-20 16:55:41 -0500

    feat: add starter files

---
+++ src/data/skills.ts
@@ -0,0 +1,12 @@
+export const skillsData = [
+  { id: 1, text: 'HTML', fontSize: '40px', color: '#e1b941' },
+  { id: 2, text: 'CSS', fontSize: '52px', color: '#4169e1' },
+  { id: 3, text: 'Sass', fontSize: '32px', color: '#e1b941' },
+  { id: 4, text: 'React', fontSize: '50px', color: '#4169e1' },
+  { id: 5, text: 'Vue', fontSize: '42px', color: '#e1b941' },
+  { id: 6, text: 'TypeScript', fontSize: '48px', color: '#4169e1' },
+  { id: 7, text: 'PHP', fontSize: '40px', color: '#e1b941' },
+  { id: 8, text: 'Python', fontSize: '48px', color: '#4169e1' },
+  { id: 9, text: 'SQL', fontSize: '36px', color: '#e1b941' },
+  { id: 10, text: 'Angular', fontSize: '20px', color: '#4169e1' },
+];
