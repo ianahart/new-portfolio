4b91cb91e2b45be44ddba9db864841f2e7d43a27
Ian Hart <ianhart7790@gmail.com>
2023-11-21 18:25:21 -0500

    feat: add skills to index page

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -10,3 +10,18 @@ export const skillsData = [
   { id: 9, text: 'SQL', fontSize: '36px', color: '#e1b941' },
   { id: 10, text: 'Angular', fontSize: '20px', color: '#4169e1' },
 ];
+
+export const frontEndSkills = [
+  { text: 'HTML', icon: 'fa-brands fa-html5' },
+  { text: 'CSS', icon: 'fa-brands fa-css3-alt' },
+  { text: 'React', icon: 'fa-brands fa-react' },
+  { text: 'Vue', icon: 'fa-brands fa-vuejs' },
+  { text: 'JavaScript', icon: 'fa-brands fa-js' },
+];
+
+export const backendSkills = [
+  { text: 'Java', icon: 'fa-brands fa-java' },
+  { text: 'PHP', icon: 'fa-brands fa-php' },
+  { text: 'Python', icon: 'fa-brands fa-python' },
+  { text: 'PostgreSQL', icon: 'fa-solid fa-database' },
+];
